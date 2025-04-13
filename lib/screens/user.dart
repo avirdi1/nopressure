8a6e20bfc5311aac4591bdda03aab9baa54d57fb
Anmol Virdi <anmolvirdi20@gmail.com>
2025-04-13 15:31:41 -0700

    changed basic layout of user

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -3,44 +3,41 @@ import 'package:flutter/material.dart';
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
 
+  Widget buildPageHeader(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 40),
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 1, 65),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Text(
+            title.toUpperCase(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple[100],
-      body: Stack(
-        children: [
-          Positioned(
-            top: 100, 
-            left: 15,
-            right: 15, 
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 129, 129, 129),
-                borderRadius: BorderRadius.circular(30),  
-              ),
-              child: const Text(
-                "User",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),  
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 400, 
-            left: MediaQuery.of(context).size.width / 2 - 60, 
-            child: ElevatedButton(
-              onPressed: () {
-                // TODO: Add logic
-              },
-              child: const Text("User Settings"),
-            ),
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            buildPageHeader("User"),
+            const Spacer(),
+            const SizedBox(height: 40),
+          ],
+        ),
       ),
     );
   }
