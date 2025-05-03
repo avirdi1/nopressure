66642b628838b74b0cedef6b5ffba15aa9b54485
Anmol Virdi <anmolvirdi20@gmail.com>
2025-05-02 19:52:05 -0700

    update main to use dotenv for api keys

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp( 
     options: DefaultFirebaseOptions.currentPlatform,
   );
