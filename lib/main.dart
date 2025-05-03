a1e9dc92ffc076a968764f3faa58ffbc2d92d9f2
Anmol Virdi <anmolvirdi20@gmail.com>
2025-05-02 19:51:04 -0700

    update gitignore, main, pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp( 
